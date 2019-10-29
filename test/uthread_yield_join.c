5aa669f399950e114624f8ed53a5f0b971b53203
ericXin95616 <xinbochao97@outlook.com>
2019-10-29 01:58:04 -0700

    delete old test file; add new test file

---
+++ test/uthread_yield_join.c
@@ -0,0 +1,54 @@
+/*
+ * Thread creation and yielding test
+ *
+ * Tests the creation of multiples threads and the fact that a parent thread
+ * should get returned to before its child is executed. The way the printing,
+ * thread creation and yielding is done, the program should output:
+ *
+ * thread1
+ * thread2
+ * thread3
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../libuthread/uthread.h"
+
+int thread4(void* arg)
+{
+	uthread_yield();
+	printf("thread%d\n", uthread_self());
+	return 0;
+}
+
+int thread3(void* arg)
+{
+	uthread_create(thread4, NULL);
+	uthread_yield();
+	printf("thread%d\n", uthread_self());
+	return 0;
+}
+
+int thread2(void* arg)
+{
+	uthread_join(uthread_create(thread3, NULL), NULL);
+	uthread_yield();
+	printf("thread%d\n", uthread_self());
+	uthread_yield();
+	return 0;
+}
+
+int thread1(void* arg)
+{
+	uthread_join(uthread_create(thread2, NULL), NULL);
+	printf("thread%d\n", uthread_self());
+	// uthread_yield();
+	return 0;
+}
+
+int main(void)
+{
+	uthread_join(uthread_create(thread1, NULL), NULL);
+	return 0;
+}
